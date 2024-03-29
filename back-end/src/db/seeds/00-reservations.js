bf3619aae3b834eabf110c918601ac1187939c98
Stephen Walker <35walke774@gmail.com>
2024-03-29 13:45:17 -0400

    second commit

--- back-end/src/db/seeds/00-reservations.js
+++
@@ -1,9 +0,0 @@
-const reservationsData = require("./00-reservations.json");
-
-exports.seed = function (knex) {
-  return knex("reservations")
-    .del()
-    .then(function () {
-      return knex("reservations").insert(reservationsData);
-    });
-};
--- back-end/src/errors/methodNotAllowed.js
+++
@@ -1,8 +0,0 @@
-function methodNotAllowed(req, res, next) {
-  next({
-    status: 405,
-    message: `${req.method} not allowed for ${req.orginalUrl}`,
-  });
-}
-
-module.exports = methodNotAllowed;
