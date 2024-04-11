b7973a7b0ec41f3faccdb2a012ecbe7feeb4ea0f
Stephen Walker <35walke774@gmail.com>
2024-04-11 18:43:47 -0400

    knexfile

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -4,11 +4,11 @@
  * You will not need to make changes to this file.
  */
 
-require('dotenv').config();
+require("dotenv").config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL,
   DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
   DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
